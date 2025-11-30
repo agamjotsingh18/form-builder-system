4231f4a657408f2e6b5e931ff431420e01c3ed19
agamjotsingh18 <70067726+agamjotsingh18@users.noreply.github.com>
2025-12-01 00:46:46 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,6 +9,6 @@ export default defineConfig({
   // Ensure the base path is correct for Netlify deployment
   base: '/', 
   build: {
-    outDir: 'dist', // Standard output directory
+    outDir: 'frontend', // Standard output directory
   }
 });
